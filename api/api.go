f677f384d9cbf36c84a0c25ea5d63cb7a2b7dc28
unknown <jiwei_m@cyou-inc.com>
2018-11-28 21:08:31 +0800

    wx init

---
+++ api/api.go
@@ -0,0 +1,19 @@
+package api
+
+import (
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"net/http"
+	"github.com/jwei2006/gwx/context"
+	"github.com/jwei2006/gwx/util"
+)
+
+func checkSignature(wechatCtx context.Context, c *gin.Context)  {
+	signature := c.Query("signature")
+	timestamp := c.Query("timestamp")
+	nonce := c.Query("nonce")
+	echostr := c.Query("echostr")
+	s := util.Signature(timestamp, nonce, wechatCtx.Token)
+	fmt.Println(signature == s)
+	c.String(http.StatusOK, echostr)
+}
